89ff0270d7f7a72118fd460fd042d67a2a5cf2ef
Iago Engel Serafim <iagoengel@yahoo.com>
2024-06-29 17:56:55 -0300

    Aula 7(Finalizando a aplicação)

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import style from './Botao.module.scss'
 
-class Botao extends React.Component<any, { type?: "button" | "submit" | "reset" | undefined }> {
-
-    render() {
-        const { type = "button" } = this.props
-
-        return (
-            <button className={style.botao} type={type}>
-                {this.props.children}
-            </button>
-        )
-    }
+interface IProps {
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void,
+    children?: React.ReactNode
 }
+
+function Botao({ onClick, type, children }: IProps) {
+    return (
+        <button className={style.botao} type={type} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+
+// class Botao extends React.Component<any, { type?: "button" | "submit" | "reset" | undefined, onClick?: () => void }> {
+
+//     render() {
+//         const { type = "button", onClick } = this.props
+
+//         return (
+//             <button className={style.botao} type={type} onClick={onClick}>
+//                 {this.props.children}
+//             </button>
+//         )
+//     }
+// }
 
 export default Botao;
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -3,27 +3,40 @@ import { Relogio } from "./Relogio";
 import style from './Cronometro.module.scss';
 import { tempoParaSegundos } from "../../common/utils/time";
 import { ITarefa } from "../../types/tarefas";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface IProps {
-    selecionado: ITarefa | undefined
+    selecionado: ITarefa | undefined,
+    finalizarTarefa: () => void
 }
 
-export default function Cronometro({ selecionado }: IProps) {
+export default function Cronometro({ selecionado, finalizarTarefa }: IProps) {
     const [tempo, setTempo] = useState<number>();
 
-    if (selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado?.tempo))
+    useEffect(() => {
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+    }, [selecionado]) // segundo parâmetro são as dependencias q vão ser 'vigiadas'. Sempre que elas mudarem, useEffect vai executar o primeiro parametro
+
+    function regressiva(contador: number = 0) {
+
+        setTimeout(() => {
+            if (contador > 0) {
+                setTempo(contador - 1)
+                return regressiva(contador - 1)
+            }
+            finalizarTarefa();
+        }, 1000)
     }
 
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronônometro</p>
-            Tempo: {tempo}
             <div className={style.relogioWrapper}>
-                <Relogio />
+                <Relogio tempo={tempo} />
             </div>
-            <Botao>
+            <Botao onClick={() => { regressiva(tempo) }}>
                 Começar
             </Botao>
         </div>
